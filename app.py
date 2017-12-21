4a459f5ca718601077df949010619783d4d47bd9
Heather Song <hsong89@gatech.edu>
2017-12-20 22:03:41 -0500

    rulesXIV

--- app.py
+++ app.py
@@ -24,6 +24,32 @@ rulesXIII = ["XIII.A. If the disc is on the ground, whether in- or out-of-bounds
 "XIII.A.4.c.If the disc is out-of-bounds, after twenty seconds elapse, a defensive player within three meters of the spot the disc is to be put into play may announce disc in, and then initiate and continue the stall count, but only if a defensive player has given audible warnings of twenty, ten and five seconds (the pre-stall)."
 "XIII.A.5.If an offensive player unnecessarily delays putting the disc into play in violation of rule XIX.B, a defender within three meters of the spot the disc is to be put into play may issue a delay of game warning instead of calling a violation. If the behavior in violation of rule XIX.B is not immediately stopped, the marker may initiate and continue a stall count, regardless of the actions of the offense. In order to invoke this rule, after announcing delay of game, the marker must give the offense two seconds to react to the warning, and then announce disc in before initiating the stall count.",
 "XIII.B.For a live disc to be put into play, the thrower must establish a pivot at the appropriate spot on the field, touch the disc to the ground, and put it into play."]
+rulesXIV = ["XIV.A. Stalling: The period of time within which a thrower must release a throw may be timed by the stall count.",
+            "XIV.A.1. The stall count consists of announcing stalling and counting from one to ten loudly enough for the thrower to hear.",
+            "XIV.A.1.a. The interval between the first utterance of each number in the stall count must be at least one second.",
+            "XIV.A.1.b. All stall counts initiated, reinitiated or resumed after a stoppage of play must start with the word stalling.",
+            "XIV.A.1.c. If the count resets to one during a stoppage of play, it is considered a new count.",
+            "XIV.A.2. Only the marker may initiate or continue a stall count, and may do so anytime a thrower has possession of a disc that is live or in play. However, directly after a turnover or thwen putting the pull into play the stall may not be initiated before a pivot is established, unless delay of game or pre-stall rules(XIII.A.3, XIII.A.4) apply.",
+            "XIV.A.3. If the thrower has not released the disc at the first utterance of the word ten, it is a turnover. The marker loudly announces stall and play stops. A stall is not a violation and rule XVI.C does not apply.",
+            "XIV.A.3.a. The marker calling the stall takes possession of the disc where the stall occurred and then may either:",
+            "XIV.A.3.a.1. place the disc on the ground and after acknowledgment by the defense, touch the disc and loudly announce in play or",
+            "XIV.A.3.a.2. retain possession and have the former thrower restart play with a check.",
+            "XIV.A.3.b. The thrower may contest a stall call in the belief that the disc was released before the first utterance of the word ten. If a stall is contested:",
+            "XIV.A.3.b.1. If the pass was complete, play stops and possession reverts to the thrower. After a check, the marker resumes the stall count at 8.",
+            "XIV.A.3.b.2. If the pass was incomplete, it is a turnover; play stops and resumes with a check.",
+            "XIV.A.4. If the defense switches markers, the new marker must reinitiate the stall count . A marker leaving the three-meter radius and returning is considered a new marker.",
+            "XIV.A.5. If a stall count is interrupted by a call, the thrower and marker are responsible for agreeing on the correct count before the check.  The count reached is the last number fully uttered by the marker before the call. The count is resumed with the word stalling followed by the number listed below:",
+            "XIV.A.5.a.1. General rules: 1. Uncontested defensive foul or violation: 1. ",
+            "XIV.A.5.a.2. Uncontested offensive foul or violation: Count reached plus 1, or 9 if over 8",
+            "XIV.A.5.a.3. Contested foul or violation: Count reached plus 1, or 6 if over 5",
+            "XIV.A.5.a.4. Offsetting calls: Count reached plus 1, or 6 if over 5",
+            "XIV.A.5.a.5. Unresolved calls: Count reached plus 1, or 6 if over 5",
+            "XIV.A.5.b.1. Specific Rules: Pick: Count reached plus 1, or 6 if over 5",
+            "XIV.A.5.b.2. Marking violation (no stoppage): Count reached minus 1, no stalling",
+            "XIV.A.5.b.3. Contested stall: First Call:8, Second and Subsequent calls when due to a fast count: 6",
+            "XIV.A.5.b.4. Defensive technical time-out: Count reached plus 1, or 6 if over 5",
+            "XIV.A.5.b.5. Offensive technical time-out: Count reached plus 1, or 9 if over 8",
+            "XIV.A.5.b.6. Obstruction within 5 meters of playing field: Count reached plus 1, or 9 if over 8"]
 @app.route('/', methods=['POST'])
 
 
@@ -55,7 +81,7 @@ def webhook():
             add_to_db(names, "gym")
             total = getTotal()
             rulePointer = total % 10
-            rule = rulesXIII[rulePointer]
+            rule = rulesXIV[rulePointer]
             send_wreck_message(rule)
         if '!throw' in text:
             #send_debug_message("throw detected")
@@ -75,7 +101,7 @@ def webhook():
             add_to_db(names, "throw")
             total = getTotal()
             rulePointer = total % 10
-            rule = rulesXIII[rulePointer]
+            rule = rulesXIV[rulePointer]
             send_wreck_message(rule)
         """
         elif '!leaderboard' in text: #post the leaderboard in the groupme
